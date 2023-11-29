2f33f6544dabfd46b3c183969d791a23ed01eb4c
Aleksandr <pulatov.alexander@gmail.com>
2023-11-29 13:36:47 +0300

    Added text color change when clicking the go button on the main page

--- L23/ViewController.swift
+++ L23/ViewController.swift
@@ -15,6 +15,12 @@ final class ViewController: UIViewController {
     // MARK: - @IBAction
     @IBAction func nextScreenButtonTapped(_ sender: Any) {
         
+        let text = textLabel.text ?? ""
+        let attributedText = NSMutableAttributedString(string: text)
+        let range = (text as NSString).range(of: text)
+        
+        attributedText.addAttribute(.foregroundColor, value: UIColor.red, range: range)
+        textLabel.attributedText = attributedText
     }
     
     override func viewDidLoad() {
