3fe0e88a272a55c2b881a48074bb2f26a9ab0b30
Aleksandr <pulatov.alexander@gmail.com>
2023-11-29 13:48:57 +0300

    Added SecondViewController and the function of saving the entered text

---
+++ L23/View/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  L23
+//
+//  Created by Alexander on 29.11.23.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var textFieldView: UITextField!
+    
+    @IBAction private func saveButtonTapped(_ sender: Any) {
+        textLabel.text = textFieldView.text ?? "" 
+    }
+
+}
